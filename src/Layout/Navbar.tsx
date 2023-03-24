2e8a7230e74fa8b6334a02dced99b9d0ee5ef7ee
bivas-biswas <bivasbiswas1999@gmail.com>
2023-03-24 16:44:38 +0530

    refactor navbar

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -1,3 +1,4 @@
+import Logo from 'assets/logo.svg'
 import clsx from 'clsx'
 import { motion } from 'framer-motion'
 import React from 'react'
@@ -31,14 +32,10 @@ const Navbar = () => {
       <nav className="max-w-3xl mx-auto px-4 py-3 flex flex-row justify-between">
         <Link to={'/'} className="text-xl">
           <motion.div
-            whileHover={{ scale: 1.1 }}
-            className="w-[60px]"
+            whileHover={{ scale: 1.03 }}
+            className="w-[60px] h-[65px]"
             initial={false}>
-            <img
-              width={60}
-              src={'/logo192.png'}
-              alt={'Duplicate Character Remover App logo'}
-            />
+            <img src={Logo} alt={'Duplicate Character Remover App logo'} />
           </motion.div>
         </Link>
         <motion.ul className="flex flex-row gap-1 items-center" layout>
@@ -52,7 +49,7 @@ const Navbar = () => {
               initial={false}
               animate={{
                 backgroundColor:
-                  pathName === route.id ? 'rgb(55, 65, 81)' : 'transparent'
+                  pathName === route.to ? 'rgb(55, 65, 81)' : 'transparent'
               }}
               className={clsx(
                 'text-base text-gray-300 px-2 py-1 rounded-sm flex items-center justify-center'
--- src/hooks/usePathName.tsx
+++ src/hooks/usePathName.tsx
@@ -2,11 +2,7 @@ import { useLocation } from 'react-router-dom'
 
 const usePathName = () => {
   const location = useLocation()
-  const pathname = location.pathname
-
-  if (pathname === '/') return 'home'
-
-  return pathname.slice(1)
+  return location.pathname
 }
 
 export default usePathName
